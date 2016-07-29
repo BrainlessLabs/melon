8f114472cef04807880d507e1775ef26d21df017
BrainlessLabs <brainlesslabs@gmail.com>
2016-07-29 21:11:03 +0000

    Initial Version with libraries

---
+++ melon/include/blib/melon.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "blib/sqlite_cpp/sqlite_modern_cpp.hpp"
+
+namespace blib {
+  namespace melon {
+  }
+}
--- melon/melon/melon.cpp
+++ melon/melon/melon.cpp
@@ -1,9 +1,8 @@
 // melon.cpp : Defines the entry point for the console application.
 //
+#include "blib/melon.hpp"
+//#include "blib/duktape/duktape.hpp"
+#define CATCH_CONFIG_MAIN
+#include <blib/catch/catch.hpp>
 
-
-int main()
-{
-    return 0;
-}
 
